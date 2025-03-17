1b1c31b981c482830bf038f4a2477d7736b32039
Lucas dechavanne <ldechavanne@outlook.fr>
2025-03-17 11:10:07 +0100

    full env

--- frontend/app.py
+++ frontend/app.py
@@ -1,37 +1,18 @@
 import streamlit as st
 import requests
-from PIL import Image
-import io 
 
-st.title("Classification d'image de fruits 🍎🍌🍇")
+st.title("Hackathon")
 
-uploaded_file = st.file_uploader(
-    "Dépose une image (PNG ou JPG)", 
-    type=["png", "jpg", "jpeg"]
-)
+st.write("Hello !")
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Image téléchargée", use_container_width=True)
+# Appel à l'API FastAPI
+backend_url = "http://backend:8000/"  # URL du service backend dans Docker
 
-    API_URL = "http://backend:8000/predict"
-    files = {"file": uploaded_file.getvalue()}
-    response = requests.post(API_URL, files=files)
-
+try:
+    response = requests.get(backend_url)
     if response.status_code == 200:
-        data = response.json()
-        st.subheader("Résultat de la prédiction :")
-        prediction = data["predictions"][0]["label"]
-        confidence = data["predictions"][0]["confidence"]
-        st.write(f"Prédiction : **{prediction}** avec confiance **{confidence:.2f}**")
-
-        # Boutons de feedback
-        if st.button("👍 Correct"):
-            requests.post(f"http://backend:8000/feedback", json={"label": prediction, "feedback": "positive"})
-            st.success("Merci pour votre feedback !")
-
-        if st.button("👎 Incorrect"):
-            requests.post(f"http://backend:8000/feedback", json={"label": prediction, "feedback": "negative"})
-            st.warning("Feedback enregistré, merci !")
+        st.success(f"Réponse de l'API : {response.json()['message']}")
     else:
-        st.error("Erreur : Impossible de traiter l'image")
+        st.error(f"Erreur {response.status_code} lors de l'appel à l'API")
+except requests.exceptions.ConnectionError:
+    st.error("Impossible de contacter l'API. Vérifiez si le backend est démarré.")
